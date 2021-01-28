8d90260b732adef99523f2b68dd22d4ed28227d1
Raghav Garg <42090624+Raghav117@users.noreply.github.com>
2021-01-28 15:12:25 +0530

    updation

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'UI/signupverify.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  // Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'Flutter Demo',
@@ -44,9 +44,9 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void initState() {
-    dialogloading = false;
+    // dialogloading = false;
     // loading = true;
-    _auth = FirebaseAuth.instance;
+    // _auth = FirebaseAuth.instance;
     // print(_auth);
     // login();
     super.initState();
@@ -204,145 +204,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: loading == true
-          ? Center(child: CircularProgressIndicator())
-          : SafeArea(
-              child: Form(
-                key: _loginFormKey,
-                child: Container(
-                    child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 15),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(28, 35, 20, 10),
-                        child: Container(
-                            child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "What is your \nphone number?",
-                              style: TextStyle(
-                                fontSize: 40,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            ),
-                            SizedBox(height: 30),
-                            Text(
-                              "To Continue to get an SMS confirmation to help you use Plan My Health. We would like your phone number.",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 17,
-                              ),
-                            ),
-                            SizedBox(height: 50),
-                            Container(
-                                child: Row(
-                              children: [
-                                // Container(
-                                //   width: MediaQuery.of(context).size.width / 6,
-                                //   child: Padding(
-                                //     padding: const EdgeInsets.all(8.0),
-                                //     child: TextFormField(
-                                //       style: TextStyle(
-                                //           color: Colors.black,
-                                //           fontSize: 22,
-                                //           fontWeight: FontWeight.w500),
-                                //       decoration: InputDecoration(
-                                //           hintText: '+91 ',
-                                //           hintStyle: TextStyle(
-                                //               fontSize: 22,
-                                //               fontWeight: FontWeight.w500)),
-                                //       validator: (value) {
-                                //         if (value.isEmpty) {
-                                //           return 'Please enter some text';
-                                //         }
-                                //         return null;
-                                //       },
-                                //     ),
-                                //   ),
-                                // ),
-                                Container(
-                                  width:
-                                      MediaQuery.of(context).size.width / 1.2,
-                                  child: TextFormField(
-                                    onChanged: (value) {
-                                      phn = value;
-                                    },
-                                    keyboardType: TextInputType.number,
-                                    maxLength: 10,
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 22,
-                                        fontWeight: FontWeight.w500),
-                                    decoration: InputDecoration(
-                                        hintText: 'Phone Number',
-                                        hintStyle: TextStyle(
-                                            fontSize: 22,
-                                            fontWeight: FontWeight.w500)),
-                                    validator: (value) {
-                                      if (value.isEmpty) {
-                                        return 'Please enter some text';
-                                      }
-                                      return null;
-                                    },
-                                  ),
-                                ),
-                              ],
-                            ))
-                          ],
-                        )),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                        child: GestureDetector(
-                          onTap: () {
-                            if (_loginFormKey.currentState.validate()) {
-                              setState(() {
-                                loading = true;
-                              });
-                              phoneNo = "+91" + phn;
-                              verifyPhone();
-                              // Navigator.of(context).pushReplacement(
-                              //     MaterialPageRoute(builder: (context) {
-                              //   return SignUpVerifyNumber(
-                              //     number: phn,
-                              //   );
-                              // }));
-                              // apiHelper.mobileLogin(context, _mobileController.text);
-                              //     .then((value) {
-                              //   Navigator.push(
-                              //       context,
-                              //       MaterialPageRoute(
-                              //           builder: (context) =>
-                              //               VerifyNumber(otp: value)));
-                              // });
-                            }
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: Colors.green,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(6))),
-                            alignment: Alignment.center,
-                            child: Padding(
-                              padding: const EdgeInsets.all(12.0),
-                              child: Text(
-                                loading == false ? "Continue" : "Loading...",
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.w600),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                )),
-              ),
-            ),
-    );
+    return Splash();
   }
 }
