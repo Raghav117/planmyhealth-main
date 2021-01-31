7e9cd465209f1093f812b2ea79aefe90c37f0948
Raghav Garg <42090624+Raghav117@users.noreply.github.com>
2021-01-31 08:15:23 -0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -166,7 +166,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Prescription();
+    return Splash();
     // Scaffold(
     //   body: ListView(
     //     children: [
