2dfbddabcba167b0d42cd6ff28650ab16cb67ebf
Raghav Garg <42090624+Raghav117@users.noreply.github.com>
2021-02-07 16:05:09 +0530

    improvements

--- lib/UI/editProfile.dart
+++ lib/UI/editProfile.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:plan_my_health/UI/pdfOpener.dart';
+import 'dart:convert';
+import 'dart:io';
+import '../model/doctorsCheckup.dart';
+import '../global/global.dart' as global;
 
 class EditProfile extends StatefulWidget {
   @override
@@ -6,12 +12,295 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
+  List<Map> m;
+  bool loading = true;
+  List<DoctorsCheckUp> doctorcheckup = List();
+  List<UserData> userData = List();
+  bool show = false;
+  var data;
+  getPrescriptionData() async {
+    var response =
+        await http.get("http://3.15.233.253:5000/doctorprecriptionlist");
+    data = jsonDecode(response.body);
+    print(data);
+    data["doctorlist"].forEach((element) {
+      element["doctorscheckup"].forEach((ele) {
+        doctorcheckup.add(DoctorsCheckUp.fromJson(ele));
+      });
+      element["userdata"].forEach((ele) {
+        userData.add(UserData.fromJson(ele));
+      });
+    });
+    print(doctorcheckup);
+    print(userData);
+    // print(userData[0].id);
+    loading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getPrescriptionData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Edit Profile"),
-      ),
+      body: loading == true
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : SafeArea(
+              child: SingleChildScrollView(
+                child: Container(
+                  decoration: BoxDecoration(
+                      // color: Colors.green,
+                      border: Border.all(color: Colors.green, width: 3),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: Image.network(
+                            global.data.picture,
+                            errorBuilder: (context, error, stackTrace) =>
+                                Icon(Icons.account_box_outlined),
+                          ),
+                          title: Text(global.data.name),
+                          subtitle: Text(global.data.email),
+                          trailing: Text(global.data.experience.toString() +
+                              " Years Experience"),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(child: Text("Mobile Number")),
+                            Expanded(
+                              child: Text(global.data.mobile.toString()),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(child: Text("Address")),
+                            Expanded(
+                              child: Text(global.data.address.toString()),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(child: Text("City")),
+                            Expanded(
+                              child: Text(global.data.cityId.toString()),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(child: Text("Clinic Name")),
+                            Expanded(
+                              child: Text(global.data.clinicname.toString()),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(child: Text("Date of Birth")),
+                            Expanded(
+                              child: Text(global.data.dob.toString()),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(child: Text("Mode of Services")),
+                            Expanded(
+                              child:
+                                  Text(global.data.modeofservices.toString()),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(child: Text("Qualification")),
+                            Expanded(
+                              child: Text(global.data.qualification.toString()),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(child: Text("Specialization")),
+                            Expanded(
+                              child:
+                                  Text(global.data.specialization.toString()),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(child: Text("Registration Number")),
+                            Expanded(
+                              child:
+                                  Text(global.data.registrationno.toString()),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            show = !show;
+                            setState(() {});
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.green,
+                                borderRadius: BorderRadius.circular(20)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(18.0),
+                              child: Text(
+                                "Prescription List",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        show == false
+                            ? Container()
+                            : Column(
+                                children: doctorcheckup.map((e) {
+                                int index = doctorcheckup.indexOf(e);
+                                return Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: InkWell(
+                                    onTap: () {
+                                      Navigator.of(context)
+                                          .push(MaterialPageRoute(
+                                        builder: (context) => PdfOpener(
+                                          url: "http://3.15.233.253/" +
+                                              data["doctorlist"][index]
+                                                      ["pdffile"]
+                                                  .replaceAll(
+                                                      "/var/www/html/", ""),
+                                        ),
+                                      ));
+                                    },
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                          border: Border.all(
+                                              color: Colors.green, width: 3)),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Column(
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Expanded(
+                                                  child: Text("Name"),
+                                                ),
+                                                Expanded(
+                                                  child: Text(
+                                                      userData[index].name),
+                                                ),
+                                              ],
+                                            ),
+                                            SizedBox(
+                                              height: 20,
+                                            ),
+                                            Row(
+                                              children: [
+                                                Expanded(
+                                                  child: Text("Mobile Number"),
+                                                ),
+                                                Expanded(
+                                                  child: Text(userData[index]
+                                                      .mobile
+                                                      .toString()),
+                                                ),
+                                              ],
+                                            ),
+                                            SizedBox(
+                                              height: 20,
+                                            ),
+                                            Row(
+                                              children: [
+                                                Expanded(
+                                                  child: Text("Date"),
+                                                ),
+                                                Expanded(
+                                                  child: Text(userData[index]
+                                                      .dateOfJoining
+                                                      .toString()),
+                                                ),
+                                              ],
+                                            ),
+                                            SizedBox(
+                                              height: 20,
+                                            ),
+                                            Row(
+                                              children: [
+                                                Expanded(
+                                                  child: Text("Diagnosis"),
+                                                ),
+                                                Expanded(
+                                                  child: Text(
+                                                      doctorcheckup[index]
+                                                          .diagnosis
+                                                          .toString()),
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              }).toList())
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
     );
   }
 }
---
+++ lib/model/doctorsCheckup.dart
@@ -0,0 +1,141 @@
+class DoctorsCheckUp {
+  String doctorid;
+  String doctorname;
+  String medicine;
+  String test;
+  String diagnosis;
+  String hospitalised;
+  String specialist;
+  String wellness;
+  String remark;
+  List<String> findings;
+  String followupdate;
+
+  DoctorsCheckUp(
+      {this.doctorid,
+      this.doctorname,
+      this.medicine,
+      this.test,
+      this.diagnosis,
+      this.hospitalised,
+      this.specialist,
+      this.wellness,
+      this.remark,
+      this.findings,
+      this.followupdate});
+
+  DoctorsCheckUp.fromJson(Map<String, dynamic> json) {
+    doctorid = json['doctorid'];
+    doctorname = json['doctorname'];
+    medicine = json['medicine'];
+    test = json['test'];
+    diagnosis = json['diagnosis'];
+    hospitalised = json['hospitalised'];
+    specialist = json['specialist'];
+    wellness = json['wellness'];
+    remark = json['remark'];
+    findings = json['findings'].cast<String>();
+    followupdate = json['followupdate'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['doctorid'] = this.doctorid;
+    data['doctorname'] = this.doctorname;
+    data['medicine'] = this.medicine;
+    data['test'] = this.test;
+    data['diagnosis'] = this.diagnosis;
+    data['hospitalised'] = this.hospitalised;
+    data['specialist'] = this.specialist;
+    data['wellness'] = this.wellness;
+    data['remark'] = this.remark;
+    data['findings'] = this.findings;
+    data['followupdate'] = this.followupdate;
+    return data;
+  }
+}
+
+class UserData {
+  String sId;
+  String id;
+  String name;
+  String cityId;
+  int pincode;
+  String gender;
+  int age;
+  int mobile;
+  String password;
+  String dateOfJoining;
+  String employeeTag;
+  int employeeId;
+  String groupId;
+  String relation;
+  String email;
+  String renewalFlag;
+  String activeFlag;
+  String activePlanId;
+
+  UserData(
+      {this.sId,
+      this.id,
+      this.name,
+      this.cityId,
+      this.pincode,
+      this.gender,
+      this.age,
+      this.mobile,
+      this.password,
+      this.dateOfJoining,
+      this.employeeTag,
+      this.employeeId,
+      this.groupId,
+      this.relation,
+      this.email,
+      this.renewalFlag,
+      this.activeFlag,
+      this.activePlanId});
+
+  UserData.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    id = json['id'];
+    name = json['name'];
+    cityId = json['cityId'];
+    pincode = json['pincode'];
+    gender = json['gender'];
+    age = json['age'];
+    mobile = json['mobile'];
+    password = json['password'];
+    dateOfJoining = json['dateOfJoining'];
+    employeeTag = json['employeeTag'];
+    employeeId = json['employeeId'];
+    groupId = json['groupId'];
+    relation = json['relation'];
+    email = json['email'];
+    renewalFlag = json['renewalFlag'];
+    activeFlag = json['activeFlag'];
+    activePlanId = json['activePlanId'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['cityId'] = this.cityId;
+    data['pincode'] = this.pincode;
+    data['gender'] = this.gender;
+    data['age'] = this.age;
+    data['mobile'] = this.mobile;
+    data['password'] = this.password;
+    data['dateOfJoining'] = this.dateOfJoining;
+    data['employeeTag'] = this.employeeTag;
+    data['employeeId'] = this.employeeId;
+    data['groupId'] = this.groupId;
+    data['relation'] = this.relation;
+    data['email'] = this.email;
+    data['renewalFlag'] = this.renewalFlag;
+    data['activeFlag'] = this.activeFlag;
+    data['activePlanId'] = this.activePlanId;
+    return data;
+  }
+}
